d14f0e17379d270a34e924c994a0bb5f5ab92882
jackbui <tambh@youngworld.vn>
2015-12-08 18:01:35 +0700

    Dev

---
+++ template.php
@@ -0,0 +1,17 @@
+<?php
+if( !function_exists('inforwindowTemplate') ){
+   function inforwindowTemplate(){
+        $template = '<div class="infowindow" ><div class="post-item"><div class="place-wrapper">
+            <a href="{{= permalink }}" class="img-place">
+                <img src="{{= the_post_thumnail }}">
+            </a>
+            <div class="place-detail-wrapper">
+                <h2 class="title-place"><a href="{{= permalink }}">{{= post_title }}</a></h2>
+                <span class="address-place"><i class="fa fa-map-marker"></i> {{= et_full_location }}</span>
+                <div class="rate-it" data-score="{{= rating_score }}"></div>
+            </div>
+        </div></div></div>';
+        echo '<script type="text/template" id="ae_info_content_template">' . apply_filters('jb_admap_template', $template) . '</script>';
+        echo '<div class="map-element" style="display:none"></div>';
+    }
+}
